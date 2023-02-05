274930e0ca4af05d33051c20bc535420e13eb177
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-02-04 21:53:00 -0600

    Moviendo la funcion a un helper para hacer mas simple el componente pero problema anterior sigue

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,18 +1,4 @@
-// Es mejor dejar esta funcion fuera del functional component para que no se reprocese esta funcion con cada refresh
-const getGifs = async (category) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key==${category}&limit=10`;
-  const resp = await fetch(url);
-  const { data } = await resp.json();
-
-  const gifs = data.map((img) => ({
-    id: img.id,
-    title: img.title,
-    url: img.images.downsized_medium.url,
-  }));
-
-  console.log(gifs);
-  return gifs;
-};
+import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
   // No se deberia ejecutar nunca una funcion dentro de un functional component pues se llama con cada refresh, pero
