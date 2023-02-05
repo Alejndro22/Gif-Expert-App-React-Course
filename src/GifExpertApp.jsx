675cafd5127648a08b506b37ca0ebf2c7fc96d0d
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-02-04 21:32:12 -0600

    No permitir datos repetidos y crear componente encargado de renderizar cada elemento

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,34 +1,25 @@
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
 
   const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
     setCategories((categos) => [newCategory, ...categos]);
     // otra forma es usando el callback de setState (igual que WebDev)
   };
 
   return (
     <>
-      {/* Tituto */}
       <h1>GifExpertApp</h1>
 
-      {/* Input */}
-      <AddCategory
-        // setCategories={setCategories}
-        onNewCategory={(value) => onAddCategory(value)}
-      />
+      <AddCategory onNewCategory={(value) => onAddCategory(value)} />
 
-      {/* Listado de items (gifs) */}
-      <ol>
-        {categories.map((category) => {
-          return <li key={category}>{category}</li>;
-        })}
-        {/* <li>ABC</li> */}
-      </ol>
-
-      {/* List item */}
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
     </>
   );
 };
