e8b3d2af47e5883d58dfdc18d241808c190dc6df
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-02-04 22:43:39 -0600

    Agregando la funcion en useEffect para que solo se renderice al inicio

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,9 +1,15 @@
+import { useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
-  // No se deberia ejecutar nunca una funcion dentro de un functional component pues se llama con cada refresh, pero
-  // por el momento quedara asi
-  getGifs(category);
+  // Use effect se utiliza para disparar efectos secundarios, en otras palabras,
+  // un proceso a ejecutar cuando algo suceda
+
+  // Cuando el arreglo de dependencias queda vacio, solo se ejecuta la primera vez que
+  // se construye el componente
+  useEffect(() => {
+    getGifs(category);
+  }, []);
 
   return (
     <>
